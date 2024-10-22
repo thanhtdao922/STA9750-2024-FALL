36b75aefa5d1852881c7e5658079c3b8075f046d
thanhtdao922 <daothanht922@gmail.com>
2024-10-22 14:02:18 -0400

    10/22/2024 Commit 2:02PM

--- build_site.R
+++ build_site.R
@@ -1,13 +1,22 @@
 #!/usr/bin/env Rscript
-# Install quarto
 
+# Install quarto if not installed
 if(!require("quarto")){
   install.packages("quarto")
 }
 library(quarto)
+
+# Check quarto installation
 if(!quarto::quarto_binary_sitrep()){
   stop("Something is wrong with your quarto installation. Try again.")
 }
-quarto::quarto_render(".")
+
+# Render the site to the 'docs/' folder
+quarto::quarto_render(input = ".", output_dir = "docs")
+
+# Add the 'docs' folder to Git
 system("git add docs/*")
-if(!any(grepl("rstudio", search()))){q("no")}
+
+# Commit and push changes to GitHub
+system("git commit -m 'Updated site with new HTML files'")
+system("git push origin main") 
